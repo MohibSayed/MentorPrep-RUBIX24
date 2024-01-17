fce4e81519b3cacb1d93abc20e974b00c2eb653c
Mohib Abbas Sayed <m0hibsayed1393@gmail.com>
2024-01-17 10:50:24 +0530

    React router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-
+import * as React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-
+import HomePage from './Components/HomePage/HomePage'
 function App() {
   return (
     <div className="App">
-      
+
+      <Routes>
+        <Route path="/" element={<HomePage />}/> 
+      </Routes>
+
     </div>
   );
 }
