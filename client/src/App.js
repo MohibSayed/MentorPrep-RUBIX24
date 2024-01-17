a69470449136df6f57a94f883f1c5c1e1a9afdbb
Parth Puranik <thosparth@gmail.com>
2024-01-17 14:58:22 +0530

    ddd

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import * as React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './Pages/HomePage/HomePage'
+import SearchAIPage from './Pages/SearchAIPage/SearchAIPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
 
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/SearchAi" element={<SearchAIPage />} />
       </Routes>
 
     </div>
---
+++ client/src/Components/IconGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './IconGrid.css'
+import { MdOutlineDataExploration } from "react-icons/md";
+export default function IconGrid() {
+  return (
+    <div className="singleIcon">
+    <MdOutlineDataExploration  style={{ fontSize: '30px' }}/>
+    Data Science
+    </div>
+  )
+}
