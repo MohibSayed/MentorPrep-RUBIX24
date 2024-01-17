31706bffc551ad22d45d36810c823c9ffd455a2e
Hamza Sayyed <hamzawp404@gmail.com>
2024-01-17 10:59:44 +0530

    Removed extra code

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
 import HomePage from './Components/HomePage/HomePage'
 function App() {
   return (
     <div className="App">
 
       <Routes>
-        <Route path="/" element={<HomePage />}/> 
+        <Route path="/" element={<HomePage />} />
       </Routes>
 
     </div>
