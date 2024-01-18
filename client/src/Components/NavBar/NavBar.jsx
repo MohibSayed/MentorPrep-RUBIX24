79e2190cebd3fbabbda9146b1e0c9a4a946c4e74
Hamza Sayyed <hamzawp404@gmail.com>
2024-01-18 19:46:05 +0530

    community design updated

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -2,90 +2,64 @@ import React from 'react'
 import Logo from '../../Assets/logo.png'
 import "./NavBar.css";
 const NavBar = () => {
-  return (
-    <div>
-        <header>
-            <nav className="menu--left" role="navigation">
-                <div className="menuToggle">
-                    <input type="checkbox"/>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <ul className="menuItem">
-                    <li>
-                            <a>Home</a>
-                        </li>
-                        <li>
-                            <a>Live Events</a>
-                        </li>
-                        <li>
-                            <a>Stories</a>
-                        </li>
-                        <li>
-                            <a>Ask Mentors</a>
-                        </li>
-                        <li>
-                            <a>Dashboard</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <nav>
-            <div className="navFull glass">
-            <div className="logoDiv">
-                <img src={Logo} alt="" width={20}/>
-            </div>
-                    <ul className='navList'>
-                        <li>
-                            <a>Home</a>
-                        </li>
-                        <li>
-                            <a>Live Events</a>
-                        </li>
-                        <li>
-                            <a>Stories</a>
-                        </li>
-                        <li>
-                            <a>Ask Mentors</a>
-                        </li>
-                        <li>
-                            <a>Dashboard</a>
-                        </li>
-                        {/* <li>
-                            <a>FAQs</a>
-                        </li> */}
-                        <li>
-                            <a className='blueBtn'><b>Find Your Mentor?</b></a>
-                        </li>
-                        {/* <li>
-                            <a className='greenBtn'><b>CONTACT</b></a>
-                        </li> */}
-                    </ul>
-                </div>
-            </nav>
-            {/* <nav className="menu--right" role="navigation">
-                <div className="menuToggle">
-                <input type="checkbox"/>
-                <span></span>
-                <span></span>
-                <span></span>
-                <ul className="menuItem">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Info</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Show me more</a></li>
-                </ul>
-                </div>
-            </nav> */}
-        </header>
-    
-
-        
-
-        
-    </div>
-  )
+    return (
+        <div>
+            <header>
+                <nav className="menu--left" role="navigation">
+                    <div className="menuToggle">
+                        <input type="checkbox" />
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <ul className="menuItem">
+                            <li>
+                                <a>Home</a>
+                            </li>
+                            <li>
+                                <a>Live Events</a>
+                            </li>
+                            <li>
+                                <a href='http://localhost:3001/'>Stories</a>
+                            </li>
+                            <li>
+                                <a>Ask Mentors</a>
+                            </li>
+                            <li>
+                                <a>Dashboard</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <nav>
+                    <div className="navFull glass">
+                        <div className="logoDiv">
+                            <img src={Logo} alt="" width={20} />
+                        </div>
+                        <ul className='navList'>
+                            <li>
+                                <a>Home</a>
+                            </li>
+                            <li>
+                                <a>Live Events</a>
+                            </li>
+                            <li>
+                                <a href='http://localhost:3001/' style={{ textDecoration: "none", color: "black" }}>Stories</a>
+                            </li>
+                            <li>
+                                <a>Ask Mentors</a>
+                            </li>
+                            <li>
+                                <a>Dashboard</a>
+                            </li>
+                            <li>
+                                <a className='blueBtn'><b>Find Your Mentor?</b></a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    )
 }
 
 export default NavBar
