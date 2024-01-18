7bb8599de57709fc153dc20037a0550958b8d6f0
Parth Puranik <thosparth@gmail.com>
2024-01-18 12:30:27 +0530

    community added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from './Components/HomePage/HomePage'
+import HomePage from './Pages/HomePage/HomePage'
+import SearchAIPage from "./Pages/SearchAIPage/SearchAIPage";
+import FindMentorPage from "./Pages/FindMentorPage/FindMentorPage";
+import CommunityHome from "./Pages/CommunityHome/CommunityHome";
 function App() {
   return (
     <div className="App">
 
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/SearchAI" element={<SearchAIPage />} />
+        <Route path="/findMyMentor" element={<FindMentorPage />} />
+        <Route path="/feed" element={<CommunityHome />}/>
       </Routes>
 
     </div>
--- server/routes/users.js
+++ server/routes/users.js
@@ -77,7 +77,6 @@ router.get("/friends/:userId", async (req, res) => {
 });
 
 //follow a user
-
 router.put("/:id/follow", async (req, res) => {
   if (req.body.userId !== req.params.id) {
     try {
