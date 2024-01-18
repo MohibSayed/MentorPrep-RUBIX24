2e9c0f3a9d1aa320d7103ae3a0faf268ce521237
Hamza Sayyed <hamzawp404@gmail.com>
2024-01-18 11:34:57 +0530

    room id input from user

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './HomePage.css'
+import { useNavigate } from 'react-router-dom'
+
 const HomePage = () => {
+  const [roomId, setRoomId] = React.useState('')
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/room/${roomId}`)
+  }
   return (
     <div>
       <h1>Hi, I am React!</h1>
+      <label>Enter Room Id:</label>
+      <input type="text" onChange={(e) => setRoomId(e.target.value)} />
+      <button onClick={handleClick}>Join Room</button>
     </div>
   )
 }
